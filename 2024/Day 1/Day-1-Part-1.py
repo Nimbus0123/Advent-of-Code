4d4c28037358be3d9f21f9424510514bfe3a538d
Valtteri Nieminen <vainie@utu.fi>
2024-12-01 17:15:37 +0200

    First day solved

---
+++ 2024/Day 1/Day-1-Part-1.py
@@ -0,0 +1,20 @@
+list1 = []
+list2 = []
+sum = 0
+
+with open('Day-1-input.txt', 'r') as file:
+    for line in file:
+        line = line.split()
+        first_id = int(line[0])
+        second_id = int(line[1])
+        list1.append(first_id)
+        list2.append(second_id)
+
+list1.sort()
+list2.sort()
+
+for index in range(len(list1)):
+    difference = abs(list1[index] - list2[index])
+    sum += difference
+
+print(sum)
---
+++ 2024/Day 1/Day-1-Part-2.py
@@ -0,0 +1,24 @@
+list1 = []
+list2 = []
+sum = 0
+
+with open('Day-1-input.txt', 'r') as file:
+    for line in file:
+        line = line.split()
+        first_id = int(line[0])
+        second_id = int(line[1])
+        list1.append(first_id)
+        list2.append(second_id)
+
+list1.sort()
+list2.sort()
+appearances = 0
+
+for first_number in list1:
+    for second_number in list2:
+        if first_number == second_number:
+            appearances += 1
+    sum += first_number * appearances
+    appearances = 0
+
+print(sum)
