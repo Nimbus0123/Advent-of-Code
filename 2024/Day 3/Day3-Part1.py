c137feca522f7e935dc1a8579585941b89c7608c
Valtteri Nieminen <vainie@utu.fi>
2024-12-03 09:48:34 +0200

    Day 3 solved

--- 2024/Day 3/Day3-Part1.py
+++ 2024/Day 3/Day3-Part1.py
@@ -5,7 +5,7 @@ sum = 0
 with open('Day3-input.txt', 'r') as file:
     for line in file:
         pattern = r'mul\((\d+),(\d+)\)'
-        matches = re.findall(pattern, line, re.IGNORECASE)
+        matches = re.findall(pattern, line)
         
         matches = [(int(a), int(b)) for a,b in matches]
         results = [a * b for a,b in matches]
