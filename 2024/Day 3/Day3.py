6a0f9edcac2fd8c0c279622a97ef517e8b3c6467
Valtteri Nieminen <vainie@utu.fi>
2024-12-03 19:06:41 +0200

    Cleaned up day 3

--- 2024/Day 3/Day3.py
+++ 2024/Day 3/Day3.py
@@ -10,19 +10,17 @@ with open('Day3-input.txt', 'r') as file:
         matches = findall(pattern, line)
 
         for match in matches:
-            if match[0].startswith("mul"):
-                num1, num2 = int(match[1]), int(match[2])
-                Part1 += num1 * num2   
 
             if match[0] == 'do()':
                 skip_multiplication = False
             elif match[0] == "don't()":
-                skip_multiplication = True                
-            elif skip_multiplication:
-                continue
+                skip_multiplication = True
+                             
             elif match[0].startswith("mul"):
                 num1, num2 = int(match[1]), int(match[2])
-                Part2 += num1 * num2
+                Part1 += num1 * num2
+                if not skip_multiplication:
+                    Part2 += num1 * num2
 
     print(f"Part 1: {Part1}")
     print(f"Part 2: {Part2}")
